b505e143440933f832b01621d380bdf5e59fe172
Vincent Weevers <mail@vincentweevers.nl>
2021-09-21 02:03:15 +0200

    Add iterate and clear benchmarks
    Undocumented for now because they're in POC stage.

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -2,4 +2,6 @@
 
 exports.put = require('./put')
 exports['batch-put'] = require('./batch-put')
+exports.iterate = require('./iterate')
+exports.clear = require('./clear')
 exports['self-distribution'] = require('./self-distribution')
